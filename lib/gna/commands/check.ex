116429d011be4964f9b819f087834764cb374548
Thibault Jouan <tj@a13.fr>
2014-11-22 19:58:44 +0000

    Implement check command basic prototype

--- lib/gna/commands/check.ex
+++ lib/gna/commands/check.ex
@@ -1,4 +1,23 @@
 defmodule Gna.Commands.Check do
-  def run(arguments) do
+  @git "git"
+  @git_commands [
+    ~w(symbolic-ref --short HEAD),
+    ~w(status --porcelain)
+  ]
+
+  def run([]) do
+    Enum.each Gna.RunControl.repos, fn(repo) ->
+      IO.puts "*** #{repo} ***"
+      Enum.each @git_commands, fn(command) ->
+        case System.cmd @git, ["-C", repo] ++ command do
+          {output, 0}           -> report output
+          {output, exit_status} -> report output
+        end
+      end
+    end
+  end
+
+  defp report(git_output) do
+    IO.write git_output
   end
 end
