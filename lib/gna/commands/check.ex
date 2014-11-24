891457cc01c5de9041085fae812284cf200ada99
Thibault Jouan <tj@a13.fr>
2014-11-24 18:39:06 +0000

    Refactor check command reporting

--- lib/gna/commands/check.ex
+++ lib/gna/commands/check.ex
@@ -6,14 +6,9 @@ defmodule Gna.Commands.Check do
     Enum.each RunControl.repos, &Repository.check(&1) |> report
   end
 
-  defp report({:ok, repository, output}) do
+  defp report({status, repository, output}) do
     IO.puts "*** #{repository} ***"
-    IO.write output
-  end
-
-  defp report({:error, repository, output}) do
-    IO.puts "*** #{repository} ***"
-    IO.puts "ERROR: cannot check repository"
+    if status == :error, do: IO.puts "ERROR: cannot check repository"
     IO.write output
   end
 end
